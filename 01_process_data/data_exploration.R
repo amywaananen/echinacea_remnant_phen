ad5608ce5f09f37c1a78cd24c6d9c2989778d67f
Arthur9474 <liu00360@umn.edu>
2021-03-04 06:41:02 +0800

    Start calculating flower time

--- 01_process_data/data_exploration.R
+++ 01_process_data/data_exploration.R
@@ -16,7 +16,31 @@ nrow(explore_sum) ###9683###
 es_withextra <- merge(explore, explore2, by = "headID", all = TRUE)
 nrow(es_withextra) ###10406###
 
+
 ###subset individuals according to their sites###
 unique(explore_sum$site)
 sitesaa <- subset(explore_sum, site == "aa", drop = FALSE)
 View(sitesaa)
+
+install.packages("dplyr")
+library(dplyr)
+
+group_by(explore_sum, site)    
+g.site <- group_by(explore_sum, site)
+View(g.site)
+
+bad <- filter(explore_sum, startDtEarly != startDtLate)
+nrow(bad)
+bad2 <- filter(explore_sum, endDtEarly != endDtLate)
+nrow(bad2)
+head(bad2)
+
+###Calculate the duration of flower time using late start time and early end time###
+###What does late and early mean?###
+
+install.packages("tidyr")
+library(tidyr)
+explore_sum %>% 
+  separate(explore_sum, col = startDtLate, into = c("yearz", "month", "day"), sep = "-")
+  separate(explore_sum, col = endDtEarly, into = c("yearz2", "month2", "day2"), sep = "-")
+View(explore_sum)
