e04fbb737a8ac0d97eed6bd40f1460a3baf669ae
Arthur9474 <liu00360@umn.edu>
2021-03-14 00:43:50 +0800

    completed the comparison of the duration, but with some obvious errors.

--- 01_process_data/data_exploration.R
+++ 01_process_data/data_exploration.R
@@ -40,7 +40,31 @@ head(bad2)
 
 install.packages("tidyr")
 library(tidyr)
-explore_sum %>% 
-  separate(explore_sum, col = startDtLate, into = c("yearz", "month", "day"), sep = "-")
-  separate(explore_sum, col = endDtEarly, into = c("yearz2", "month2", "day2"), sep = "-")
-View(explore_sum)
+select(explore_sum, headID, site, year.y, startDtEarly,startDtLate, endDtEarly, endDtLate)
+tidy <- (select(explore_sum, headID, site, year.y, startDtEarly,startDtLate, endDtEarly, endDtLate))
+
+separated <- tidy %>%
+separate(startDtLate, into = c("yearz", "month", "day"), sep = "-") %>%
+separate(endDtEarly, into = c("yearz2", "month2", "day2"), sep = "-")
+seperated %>%
+  filter(yearz != yearz2)
+
+mean(separated$month2)
+sapply(separated, mode)
+sapply(separated, class)
+
+numex <- transform(separated, month = as.numeric(month))
+mode(numex$month)
+
+sep <- seperated %>% 
+  transform(month = as.numeric(month)) %>% 
+  transform(day = as.numeric(day)) %>% 
+  transform(month2 = as.numeric(month2))%>% 
+  transform(day2 = as.numeric(day2))
+                
+dur <- sep %>% 
+  mutate(duration = (month2-month)*30 + (day2-day))
+
+Sum <- dur%>%
+  group_by(site)%>%
+  summarise(avg_duration = mean(duration))
