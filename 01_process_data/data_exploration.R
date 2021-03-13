0f77e2e86ac072be13c13cb3fe31608b6b151dd3
Arthur9474 <liu00360@umn.edu>
2021-03-14 01:03:40 +0800

    Creating a markdown document

--- 01_process_data/data_exploration.R
+++ 01_process_data/data_exploration.R
@@ -2,6 +2,14 @@
 
 ### The objective of this research is to find if there is a difference in flowering times between populations of Echinacea in Western Minnesota.###
 
+###install packages dplyr and tidyr###
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("tidyr")
+library(tidyr)
+
+### import dataset###
 explore <- read.csv("00_raw_data/headIDtoAKA20190612.csv")
 nrow(explore) ###10262###
 
@@ -19,16 +27,8 @@ nrow(es_withextra) ###10406###
 
 ###subset individuals according to their sites###
 unique(explore_sum$site)
-sitesaa <- subset(explore_sum, site == "aa", drop = FALSE)
-View(sitesaa)
 
-install.packages("dplyr")
-library(dplyr)
-
-group_by(explore_sum, site)    
-g.site <- group_by(explore_sum, site)
-View(g.site)
-
+###Check start and end date###
 bad <- filter(explore_sum, startDtEarly != startDtLate)
 nrow(bad)
 bad2 <- filter(explore_sum, endDtEarly != endDtLate)
@@ -36,35 +36,36 @@ nrow(bad2)
 head(bad2)
 
 ###Calculate the duration of flower time using late start time and early end time###
-###What does late and early mean?###
 
-install.packages("tidyr")
-library(tidyr)
+###select relevant columns only###
 select(explore_sum, headID, site, year.y, startDtEarly,startDtLate, endDtEarly, endDtLate)
 tidy <- (select(explore_sum, headID, site, year.y, startDtEarly,startDtLate, endDtEarly, endDtLate))
 
+###separate the date columns for easier calculation###
 separated <- tidy %>%
-separate(startDtLate, into = c("yearz", "month", "day"), sep = "-") %>%
-separate(endDtEarly, into = c("yearz2", "month2", "day2"), sep = "-")
-seperated %>%
+  separate(startDtLate, into = c("yearz", "month", "day"), sep = "-") %>%
+  separate(endDtEarly, into = c("yearz2", "month2", "day2"), sep = "-")
+
+###Check if there is errors in the years column###
+separated %>%
   filter(yearz != yearz2)
 
-mean(separated$month2)
+###change the separated columns to numeric from character###
 sapply(separated, mode)
 sapply(separated, class)
 
-numex <- transform(separated, month = as.numeric(month))
-mode(numex$month)
-
-sep <- seperated %>% 
+sep <- separated %>% 
   transform(month = as.numeric(month)) %>% 
   transform(day = as.numeric(day)) %>% 
   transform(month2 = as.numeric(month2))%>% 
   transform(day2 = as.numeric(day2))
-                
+
+###Calculate the duration###           
 dur <- sep %>% 
   mutate(duration = (month2-month)*30 + (day2-day))
 
+###Summarize the duration by sites###
 Sum <- dur%>%
   group_by(site)%>%
   summarise(avg_duration = mean(duration))
+
